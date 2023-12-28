2ebee260a91ba8f635760bf1cc2523b7ac5d881a
abdobassel <abdobassel@gmail.com>
2023-12-28 23:36:05 +0200

    productModel is  ready

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,8 +1,50 @@
-/*
-{   "id": 21,
-    "title": "test",
-    "price": "13.5",
-    "description": " lorem ipsum set",
-    "image": " https://i.pravatar.cc",
-    "category": "electronic"
-}*/
+class ProductModel {
+  int? id;
+  String? title;
+  String? price;
+  String? dec;
+  String? img;
+  String? category;
+  RatingModel? rating;
+  ProductModel(
+      {this.category,
+      this.id,
+      this.dec,
+      this.img,
+      this.price,
+      this.rating,
+      this.title});
+
+  ProductModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    price = json['price'];
+    category = json['category'];
+    img = json['image'];
+    dec = json['description'];
+    rating = RatingModel.fromJson(json['rating']);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'description': dec,
+      'image': img,
+      'price': price,
+      'title': title,
+      'category': category,
+      'rating': rating,
+    };
+  }
+}
+
+class RatingModel {
+  double? rate;
+  int? count;
+
+  RatingModel({this.count, this.rate});
+  RatingModel.fromJson(Map<String, dynamic> json) {
+    rate = json['id'];
+    count = json['count'];
+  }
+}
